cdcbecbf9294db3fdb253b76176ecb2fcb923a04
nathansmithsmith <thenathansmithsmith@gmail.com>
2022-08-29 15:01:57 -0600

    I forgore whaz I updated.  I know I did something?

--- lua/config_cmp.lua
+++ lua/config_cmp.lua
@@ -47,13 +47,13 @@ cmp.setup({
     documentation = cmp.config.window.bordered(),
   },
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-	["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<A-p>"] = cmp.mapping.select_prev_item(),
+	["<A-n>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping {
+    ["<A-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
